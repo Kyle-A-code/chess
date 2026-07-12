3590f62935abdedffbdd4bd9c11814ed3bc37871
= <kyle.j.annett@gmail.com>
2026-07-12 11:43:06 +1000

    persisted game state via serialized fen string in local storage

--- src/lib/game/gameController.svelte.ts
+++ src/lib/game/gameController.svelte.ts
@@ -1,23 +1,26 @@
 import type { BoardState } from '../types.ts';
-import { fenToBoardState } from './fen';
+import { fenToBoardState, boardStateToFen } from './fen';
 
 export const START_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+const FEN_STRING_STORAGE_KEY = 'fenString'
 
 interface GameState {
 	selectedTileId: string | undefined;
 	boardState: BoardState;
 }
 
-const createNewGameState = (): GameState => ({
-	selectedTileId: INITIAL_SELECTED_TILE_ID,
-	boardState: fenToBoardState(START_FEN)
-});
-
-const INITIAL_SELECTED_TILE_ID = undefined;
+const createNewGameState = (): GameState => {
+	const storedBoardState = localStorage.getItem(FEN_STRING_STORAGE_KEY)
+	return {
+		selectedTileId: undefined,
+		boardState: fenToBoardState(storedBoardState || START_FEN)
+	}
+};
 
 export const gameState = $state<GameState>(createNewGameState());
 
 export const resetGame = () => {
+	localStorage.removeItem(FEN_STRING_STORAGE_KEY)
 	const resetState = createNewGameState();
 	gameState.selectedTileId = resetState.selectedTileId;
 	gameState.boardState = resetState.boardState;
@@ -29,24 +32,27 @@ export const selectTile = (tileId: string) => {
 		return;
 	}
 
+	if (gameState.boardState.piecePlacement[tileId]?.side === gameState.boardState.activeSide) {
+		gameState.selectedTileId = tileId;
+		return
+	}
+
 	if (gameState.selectedTileId !== undefined) {
 		// TODO: this is a naive implementation implement legal move checking
+		// TODO: set half move counter, castling availability and en passant target
+
 		const targetTile = gameState.boardState.piecePlacement[tileId];
 		if (targetTile?.side === gameState.boardState.activeSide) {
 			return;
 		}
 		const selectedPiece = gameState.boardState.piecePlacement[gameState.selectedTileId];
 		gameState.boardState.piecePlacement[gameState.selectedTileId] = undefined;
+		// TODO: increment score counter by removed piece if capturing
 		gameState.boardState.piecePlacement[tileId] = selectedPiece;
 		deselectTile();
 		nextTurn();
+		persistBoardState();
 	}
-
-	if (gameState.boardState.piecePlacement[tileId]?.side !== gameState.boardState.activeSide) {
-		return;
-	}
-
-	gameState.selectedTileId = tileId;
 };
 
 const nextTurn = () => {
@@ -61,3 +67,8 @@ const nextTurn = () => {
 const deselectTile = () => {
 	gameState.selectedTileId = undefined;
 };
+
+const persistBoardState = () => {
+	const fenString = boardStateToFen(gameState.boardState)
+	localStorage.setItem(FEN_STRING_STORAGE_KEY, fenString)
+}
